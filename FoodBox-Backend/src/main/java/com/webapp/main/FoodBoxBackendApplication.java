2ad89d034f0023a50454aef3768f9397621614c5
Swapnil Pal <pal35swapnil35@gmail.com>
2022-06-04 15:22:28 +0530

    init backend creation

---
+++ FoodBox-Backend/src/main/java/com/webapp/main/FoodBoxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodBoxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodBoxBackendApplication.class, args);
+	}
+
+}
---
+++ FoodBox-Backend/src/test/java/com/webapp/main/FoodBoxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.webapp.main;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class FoodBoxBackendApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
